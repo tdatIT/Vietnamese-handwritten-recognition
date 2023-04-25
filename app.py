df521d13ad32bd9687c6468b43cac81b4f34ccdd
tdatIT <tdat.it2k2@gmail.com>
2023-04-25 18:36:33 +0700

    add use doc

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# chuyen thanh pip neu khong dung conda
+# conda activate vietnamese-ocr
+# conda install -c conda-forge opencv
+# conda install -c conda-forge tensorflow
+# conda install -c anaconda scikit-learn
+# conda install -c conda-force Matplotlib
+# conda install -c conda-forge editdistance  
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
@@ -6,10 +13,11 @@ import process_image
 import vietnamese_ocr
 
 #upload image
-valid_img = process_image.readImage('d:\\Work Or Study\\Python\\Final PID\\vietnamese_hcr\\raw\\data\\1822_samples.png')
+#thay doi anh o day
+valid_img = process_image.readImage('test_img.png')
 #prediction
 str_pred = vietnamese_ocr.prediction_ocr(valid_img)
-print('Du doan ket qua la')
+print('Du doan ket qua la:')
 print(str_pred)
 
 
