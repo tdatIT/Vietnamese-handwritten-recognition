05d16309259cd35d233f7db7a92245dd55c1cea3
tdatIT <tdat.it2k2@gmail.com>
2023-04-25 18:30:57 +0700

    remove unnecessary file

--- app.py
+++ app.py
@@ -12,8 +12,5 @@ str_pred = vietnamese_ocr.prediction_ocr(valid_img)
 print('Du doan ket qua la')
 print(str_pred)
 
-img = cv2.imread('d:\\Work Or Study\\Python\\Final PID\\vietnamese_hcr\\raw\\data\\1822_samples.png')
-plt.subplot()
-plt.imshow(img)
-plt.xlabel(f"Dự đoán: "+ str_pred, fontsize=20, color="red")
+
 
